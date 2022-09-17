ab988be9ee7051eeb5ab125e8df9a5162b724ec0
Beqlynd <bekki.marett@gmail.com>
2022-09-17 16:14:45 -0400

    Week 3 updates
    week 3 updates

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator animations;
+
+    public float speed = 5.0f;
+    public int jumpForce = 300;
+    public bool isGrounded;
+    public LayerMask isGroundedLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animations = GetComponent<Animator>();
+
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 300;
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.02f;
+        }
+        
+    }
+
+    private void OnValidate()
+    {
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 300;
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.02f;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundedLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+        rb.velocity = moveDirection;
+    }
+}
